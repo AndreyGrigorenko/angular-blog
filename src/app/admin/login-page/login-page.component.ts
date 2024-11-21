9b62dd2bb7c55c2b83083e0f71d9fbf217d8fe50
Andrii Hryhorenko <grigorenko584@gmail.com>
2024-11-21 13:46:51 +0200

    Початок роботи над сторінкою входу

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  });
 
+  submit() {
+    if (this.form.invalid) {
+
+    }
+  }
 }
